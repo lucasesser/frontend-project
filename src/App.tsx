ed3394b773cdfe97840f688e3445cc4a4a1a0705
lucasesser <lucas2002esser@gmail.com>
2026-04-23 02:33:41 -0400

    configurate tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='bg-amber-200'>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
@@ -114,7 +113,7 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
-    </>
+    </div>
   )
 }
 
