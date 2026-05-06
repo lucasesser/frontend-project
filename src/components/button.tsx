fb8268ae8721a5424e3dc8112f13277120a20a1e
lucasesser <lucas2002esser@gmail.com>
2026-05-06 03:56:20 -0400

    Working at mylinks

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,7 +4,8 @@ import { textVariants } from "./text"
 const buttonVariants = cva("", {
     variants: {
         variant: {
-            primary: ["w-full", "h-12", "rounded-lg", "text-white"]
+            primary: ["w-full", "h-12", "rounded-lg", "text-white"],
+            secondary: ["w-max", "h-8", "bg-gray-200", "rounded-sm", "flex", "justify-center", "items-center", "gap-1.5", "px-2"]
         },
         subVariant: {
             default: "",
@@ -28,15 +29,29 @@ const buttonVariants = cva("", {
             subVariant: "disabled",
             class: ["bg-blue-base", "opacity-50"]
         },
+        {
+            variant: "secondary",
+            subVariant: "hover",
+            class: ["border", "border-b-blue-base"]
+        },
+        {
+            variant: "secondary",
+            subVariant: "disabled",
+            class: ["opacity-50"]
+        }
     ]
 })
 
-interface buttonInputs extends VariantProps<typeof buttonVariants> {
+interface buttonInputs extends React.ComponentProps<"svg">, VariantProps<typeof buttonVariants> {
     children: string
+    svg?: React.FC<React.ComponentProps<"svg">>;
 }
 
-export default function Button({children, variant, subVariant}: buttonInputs) {
+export default function Button({children, variant, subVariant, svg: SvgComponent}: buttonInputs) {
     return(
-        <button className={cx(buttonVariants({variant, subVariant}), textVariants({variant: "Text Md"}))}>{children}</button>
+        <button className={cx(buttonVariants({variant, subVariant}), variant === "secondary" ? textVariants({variant: "Text Sm", className: "font-semibold"}) : textVariants({variant: "Text Md"}))}>
+            {variant === "secondary" && SvgComponent && <SvgComponent className="size-4"/>}
+            {children}
+        </button>
     )
-}   
+}
--- src/components/myLinks.tsx
+++ src/components/myLinks.tsx
@@ -1,7 +1,17 @@
+import Button from "./button";
+import Text from "./text";
+import Download from '../assets/icons/download-simple.svg?react'
+
 export default function MyLinks() {
     return(
-        <div className="flex flex-col justify-center bg-white rounded-lg max-w-145 w-full h-50">
-
+        <div className="flex flex-col justify-center p-8 bg-white rounded-lg max-w-145 w-full h-max gap-5">
+            <div className="w-full flex justify-between">
+                <Text variant="Text Lg">Meus links</Text>
+                <Button variant="secondary" subVariant="default" svg={Download}>Baixar CSV</Button>
+            </div>
+            <div className="h-29.5 border-t">
+                
+            </div>
         </div>
     )
 }
