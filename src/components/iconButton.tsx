c31abbfac136236e4600eb57ab1e2ea135b8d558
lucasesser <lucas2002esser@gmail.com>
2026-05-08 00:57:21 -0400

    feat: update IconButton to handle hover state and modify MyLinks to display LinkItem instead of NoLinks

--- src/components/iconButton.tsx
+++ src/components/iconButton.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 interface iconInputs {
     className?: string,
     svg: React.FC<React.ComponentProps<"svg">>
+    variant?: "Default" | "Hover"
 }
 
-export default function IconButton({className, svg: SvgComponent}: iconInputs) {
+export default function IconButton({className, svg: SvgComponent, variant="Default"}: iconInputs) {
+    const [hovered, setHover] = useState("")
+    const buttonClass =
+        "size-8 bg-gray-200 rounded-sm flex justify-center items-center " +
+        className + " " +
+        hovered
     return(
-        <button className={"size-8 bg-gray-200 rounded-sm flex justify-center items-center " + className}>{<SvgComponent className="fill-gray-600 size-4"/>}</button>
+        <button className={buttonClass} onMouseEnter={() => setHover("border border-b-blue-base")} onMouseLeave={() => setHover("")}>{<SvgComponent className="fill-gray-600 size-4"/>}</button>
     )
 }
